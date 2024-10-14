f05c15f487289220c6abbb70d78523a90b8654d5
kavishka-palihena <kavishka.palihena@gmail.com>
2024-10-14 17:27:14 +0530

    Initial commit

---
+++ dashboard.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+#Title of the Dashbord
+st.title("Rohana Motors")
+
+#Add a header
+st.header("Spare Parts System")
+
+st.write("This system will provide you a descriptive summary of all the inventory data and sales data of Spare Parts")
+
+#Add a slider input
+slider_value = st.slider("Select a year", 2024, 2025, 2026)
+
+#Display the slider value
+st.write(f"The selected value is {slider_value}")
+
+name = st.text_input("Enter your name")
+st.write(f"Hello, {name}!")
+
+option = st.selectbox("Select an option", ["Oil", "Motor Cycle", "Three-Wheel"])
+st.write(f"Showing {option} spare parts")
+
+if st.checkbox("Show more"):
+    st.write("Checkbox is checked!")
+
+if st.button("Click me"):
+    st.write("Button clicked!")
+
+
+import pandas as pd
+
+data = pd.read_csv("C:/Users/User/Downloads/water_potability.csv")
+df = pd.DataFrame(data)
+
+#Display the DataFrame
+st.write("Here's this year stock:")
+st.dataframe(df)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generate some data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Create a plot
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+# Display the plot
+st.pyplot(fig)
---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as sl
+
+sl.title("Obesity or CVD Prediction")
+sl.text("This app predicts whether you are obese or have cardiovascular disease")
+sl.markdown("---")
+sl.markdown("###  `sl.text(Print('Hello! World.'))`")
+sl.markdown("[Predict](http://localhost:8501/)")
+sl.markdown("[Markdown Cheat codes](https://www.markdownguide.org/cheat-sheet/)")
+sl.markdown("[Latex codes](https://katex.org/docs/supported.html)")
